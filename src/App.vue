7eac45b0c3fc8e8e6c2877633fe036c9ee3cf645
brianbarenbaum <brianbarenbaum@gmail.com>
2026-02-15 14:08:12 -0500

    fix(chat-ui): unwrap controller refs in app bindings

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@ import ChatShell from "./features/chat/components/ChatShell.vue";
 import { useChatController } from "./features/chat/useChatController";
 
 const controller = useChatController();
+const { messages, isSending, submit } = controller;
 </script>
 
 <template>
   <ChatShell
-    :messages="controller.messages"
-    :is-sending="controller.isSending"
-    @submit="controller.submit"
+    :messages="messages"
+    :is-sending="isSending"
+    @submit="submit"
   />
 </template>
